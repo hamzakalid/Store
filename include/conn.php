e457c729238bcb8ed2f947852bd0af853617171b
hamzakaled <thehamza727@gmail.com>
2020-12-11 13:04:24 +0300

    first commit

---
+++ include/conn.php
@@ -0,0 +1,9 @@
+<?php
+    //Connection Info
+    $HOST = 'localhost';
+    $USER = 'root';
+    $PASS = '';
+    $DB = 'store';
+
+    //connection instraction
+    $conn=mysqli_connect($HOST,$USER,$PASS,$DB);
